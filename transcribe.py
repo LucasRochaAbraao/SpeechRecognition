14d6a288a6cd6ff713e649c8cb9158daacab3366
LucasRochaAbraao <lucasrabraao@gmail.com>
2021-07-24 01:26:21 -0300

    reorganized the code

--- transcribe.py
+++ transcribe.py
@@ -2,24 +2,23 @@
 
 import speech_recognition as sr
 
-# ffmpeg -i audio.ogg audio.wav (to convert from .ogg to .wav)
+# ffmpeg -i audio.ogg audio.wav (para converter de .ogg (ou outro) para .wav)
 
 from os import path
 import sys
 
 AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), f"{sys.argv[1]}")
 
-# use the audio file as the audio source
+# usar AUDIO_FILE como fonte
 r = sr.Recognizer()
-with sr.AudioFile(AUDIO_FILE) as source:
-    r.adjust_for_ambient_noise(source) # if necessary
-    audio = r.record(source)  # read the entire audio file
+with sr.AudioFile(AUDIO_FILE) as fonte:
+    r.adjust_for_ambient_noise(fonte) # se necessaario
+    audio = r.record(fonte)  # le todo o arquivo de audio para a memoria
 
 #print("Áudio em memória.")
-# recognize speech using Google Speech Recognition
 try:
-    # for testing purposes, we're just using the default API key
-    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+    # Para testes, estamos usando a chave default da API
+    # par usar outra chave de API, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
 
     print(r.recognize_google(audio, language="pt-BR"))
 except sr.UnknownValueError:
